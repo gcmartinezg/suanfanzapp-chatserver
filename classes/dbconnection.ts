d12aedc7949a7f2a60ec3d140b8dee850c767c96
Daniel Vergara Lozano <josedani0630@hotmail.com>
2020-10-25 18:50:24 -0500

    bugfix para las rutas

--- classes/dbconnection.ts
+++ classes/dbconnection.ts
@@ -6,7 +6,7 @@ export const mysqlConnection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'daniel3098ñ',
+    password: 'yourpassword',
     database: 'db_suanfanzapp'
 });
 
